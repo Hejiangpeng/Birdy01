f5aa77f3645182cf1e04ab02eb3cfaef69f3f937
Hejiangpeng <15068804092@163.com>
2019-11-22 22:12:31 +0800

    LOGINFILE

--- views.py
+++ views.py
@@ -1,5 +1,9 @@
 from django.http import Httpresponse
+from django.shotcuts import redirect
 
 
 def index(request):
     return HttpResponse("INDEX")
+
+def login(request):
+    return redirect("/index")
